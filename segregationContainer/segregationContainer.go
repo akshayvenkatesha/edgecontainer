9bf0c54407cc0d5df415c0bea2f6174ba99efedd
Akshay N V <akshayvenkatesha@gmail.com>
2018-12-18 21:41:21 +0530

    added code to unmount share

--- segregationContainer/segregationContainer.go
+++ segregationContainer/segregationContainer.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 )
 
 var shareIp string
@@ -11,6 +14,10 @@ var localShare string
 var cloudShare string
 var shareUserName string
 var sharePassword string
+
+const sourceShareMountPath = "/source"
+const localShareMountPath = "/local"
+const cloudShareMountPath = "/cloud"
 
 func initFlags() {
 	flag.StringVar(&shareIp, "shareIp", "127.0.0.1", "Ip of the machine where shares are hosted.")
@@ -30,4 +37,53 @@ func main() {
 	fmt.Println(cloudShare)
 	fmt.Println(shareUserName)
 	fmt.Println(sharePassword)
+
+	createInitFolders()
+	mountShares()
+	unMountShares()
+}
+
+func createInitFolders() {
+	createFolder(sourceShareMountPath)
+	createFolder(localShareMountPath)
+	createFolder(cloudShareMountPath)
+}
+
+func createFolder(folderPath string) {
+	err := os.Mkdir(folderPath, os.ModePerm)
+	if err != nil {
+		fmt.Errorf("failed to create folder" + folderPath)
+	}
+}
+
+func mountShares() {
+	mountShare(sourceShare, sourceShareMountPath)
+	// mountShare(localShare, localShareMountPath)
+	// mountShare(cloudShare, cloudShareMountPath)
+}
+
+func mountShare(sharePath string, mountPath string) {
+	str := "mount -t cifs //" + shareIp + sharePath + " " + mountPath + " -o username=" + shareUserName + ",password=" + sharePassword
+	fmt.Println(str)
+	cmd := exec.Command("sh", "-c", str)
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func unMountShares() {
+	unMountShare(sourceShareMountPath)
+	// mountShare(localShareMountPath)
+	// mountShare(cloudShareMountPath)
+}
+
+func unMountShare(mountPath string) {
+	str := "umount " + mountPath
+	fmt.Println(str)
+	cmd := exec.Command("sh", "-c", str)
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
